9b196b038cd3963d75502aa3cedddfe52b743932
“harshpatel” <harshkpatel310@gmail.com>
2023-08-07 15:10:26 -0400

    errors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+console.log("js file connected");
+
 (function(){
 	"use strict";	
 	console.log("fired");
@@ -10,16 +12,7 @@
 		button.classList.toggle("expanded");
 	};
 
-	// let hamburgerMenu = () => {
-	// 	burgerCon.classList.toggle("slide-toggle");
-	// 	button.classList.toggle("expanded");
-	// };
 
 	button.addEventListener("click", hamburgerMenu, false);		
 })();
-
-//Can also be written like this:
-//(() => {  })();   
-
-// Info about IIFE https://flaviocopes.com/javascript-iife/
 
